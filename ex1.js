fa33a8db6b15aba0c88304ed3ffb1eaf29c08537
celiceceron <celice.cer@gmail.com>
2022-10-30 21:44:07 +0100

    codando com metodos e objetos

---
+++ ex1.js
@@ -0,0 +1,8 @@
+let filme = {
+    titulo: "O jogo da Imitação",
+    notaAvaliacao: 8,0,
+    duracao: 114,
+    anoPublicacao: 2014,
+    genero: ["Biografia", "Drama", "Suspense", "Guerra"] 
+
+};
