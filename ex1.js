05785afde49c6d8d26a3cebff1207404cc249ee3
celiceceron <celice.cer@gmail.com>
2022-10-30 21:59:06 +0100

    codando com metodos e objetos

--- ex1.js
+++ ex1.js
@@ -1,8 +1,28 @@
-let filme = {
+
+const exibeFilme = (filme) => {
+    console.log(filme.titulo);
+    console.log(filme.notaAvaliacao);
+    console.log(filme.duracao);
+    console.log(filme.anoPublicacao);
+    console.table(filme.genero); 
+};
+
+let f = {
     titulo: "O jogo da Imitação",
-    notaAvaliacao: 8,0,
+    notaAvaliacao: 8.0,
     duracao: 114,
     anoPublicacao: 2014,
     genero: ["Biografia", "Drama", "Suspense", "Guerra"] 
 
 };
+
+exibeFilme(f);
+
+
+f.titulo = "Estrelas Além do Tempo",
+f.notaAvaliacao = 7.8,
+f.duracao = 127,
+f.anoPublicacao = 2016,
+f.genero = ["Biografia", "Drama", "História"] 
+
+exibeFilme(f);
